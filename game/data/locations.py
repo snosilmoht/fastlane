9e003ea4d340b2b335869888ad75aac341dbe9ed
Snosilmoht <snosilmoht@gmail.com>
2018-08-07 20:57:05 -0400

    renamed venues to locations created location class, store class building jobs class for store class, need to add save functions will have 'towns' be buildable, storeable?

--- game/data/locations.py
+++ game/data/locations.py
@@ -1,4 +1,10 @@
-def venueData():
+'''
+Add create venue function
+custom maps storage
+'''
+
+
+def locationData():
     venueDict = {
 
                 'McDonalds' :
@@ -80,7 +86,49 @@ def venueData():
                                 }
                 }
     return venueDict
-    
+
+
+
+class Location():
+    def __init__(self, store_name, coordinates, jobs):
+        self.store_name = store_name
+        self.loc = coordinates
+
+    def save(self):
+        return
+
+    def load(self):
+        return
+
+class Store(Location):
+    def __init__(self, store_name, coordinates, jobs, shop):
+        super(Location, self).__init__(store_name, coordinates, jobs)
+
+        print self.store_name
+
+class Jobs(self):
+    def __init__(self):
+        self.jobs = {}
+
+    def add_job(self):
+        title = raw_input('title: ')
+        salary = raw_input('salary: ')
+        prestige = raw_input('prestige: ')
+        minApp = raw_input('minimum appearance 0-4: ')
+        minEdu = raw_input('min education (0-4): ')
+
+        self.jobs[title] = {'title': title,
+                            'salary': salary,
+                            'prestige': prestige,
+                            'minApp': minApp,
+                            'minEdu': minEdu}
+
+    def save(self):
+        jobData = {}
+
+        #############################################
+
+
 '''
 venues = venues()
 
